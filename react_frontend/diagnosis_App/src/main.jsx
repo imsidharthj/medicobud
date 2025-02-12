d73cc8066f0659ddc2dd044e7f0e385fa7b9b0a5
Sidharth Jain <imsidharthj@gmail.com>
2025-02-12 22:24:23 +0530

    diagnosis added redux provider to app entry point

--- react_frontend/diagnosis_App/src/main.jsx
+++ react_frontend/diagnosis_App/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { store } from './features/store.js';
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 );
