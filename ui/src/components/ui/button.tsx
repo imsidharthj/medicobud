feaf03417ad7f6fb2071b06fa27c3a60d031bee7
imsidharthj <imsidharthj@gmail.com>
2025-05-07 23:27:39 +0530

    added new veriant for blue button ghost

--- ui/src/components/ui/button.tsx
+++ ui/src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
         blueButton: "bg-blue-500 text-white hover:bg-[#105eac] cursor-pointer",
+        blueButtonGhost: "hover:bg-blue-100 hover:text-neutral-900",
       },
       size: {
         default: "h-10 px-4 py-2",
